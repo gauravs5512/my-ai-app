31a5bdede98686bfa76aaf739eb87afc6edafa79
Gaurav <gaurav.mail125@gmail.com>
2025-09-28 12:56:12 +0530

    prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
 });
 
-app.get('/api/hello', (req: Request, res: Response) => {
+app.get("/api/hello", (req: Request, res: Response) => {
   res.send({
-    message: 'Hello from the API!',
+    message: "Hello from the API!",
   });
 });
 
