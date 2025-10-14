38b39166a8be01df15ff6dd6cfce14101ac42698
Gaurav <gaurav.mail125@gmail.com>
2025-10-14 18:47:29 +0530

    allowing origins

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -6,6 +6,21 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
