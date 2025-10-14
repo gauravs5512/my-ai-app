b4a8e4c18b26157375835a607777d838678d2253
Gaurav <gaurav.mail125@gmail.com>
2025-10-14 19:11:05 +0530

    fix build error

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode, command }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const devApiUrl = env.VITE_DEV_API_URL ?? "http://localhost:8080";
   const prodApiUrl = env.VITE_PROD_API_URL ?? env.VITE_DEV_API_URL ?? devApiUrl;
